85c71dff882887ee6de0a053715d75011cb15598
ibezeke <ezlifeline@yahoo.com>
2022-07-31 03:14:27 -0700

    Just Following the Tutorial, move along

---
+++ get_test/JS/Fundamental_P1/javascript.js
@@ -0,0 +1,15 @@
+//Java goes here too
+
+console.log("Hello, Again!")
+
+let admin, name;
+name = "John";
+admin = name;
+
+alert(admin);
+
+let ourPlanetName = "Earth";
+let currentVisitor = "John";
+
+alert( ourPlanetName);
+alert(currentVisitor);
